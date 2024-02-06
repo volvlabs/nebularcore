0ed7249508ed5c52e73efcf676faaed64c7bd1da
Obasan Olajide <jideobs@gmail.com>
2024-02-06 23:21:09 +0100

    feat: add store and file validation features

--- tools/filesystem/utils.go
+++ tools/filesystem/utils.go
@@ -1,12 +1,35 @@
 package filesystem
 
 import (
+	"mime/multipart"
+	"net/http"
 	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func GetRootDir(relativePath string) string {
 	_, currentFile, _, _ := runtime.Caller(1)
 	return filepath.Join(path.Dir(currentFile), relativePath)
+}
+
+func IsValidAudioFileType(fileHeader *multipart.FileHeader) bool {
+	file, err := fileHeader.Open()
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return false
+	}
+
+	file.Seek(0, 0)
+
+	mimeType := http.DetectContentType(buffer)
+
+	return strings.HasPrefix(mimeType, "audio/")
 }
