259e2c1fd73d7c5baad569e1b6e5a3d5395c4fdf
Obasan Olajide <jideobs@gmail.com>
2024-12-10 03:28:42 +0100

    update test migration runner

--- test/migrate.go
+++ test/migrate.go
@@ -9,8 +9,7 @@ import (
 	"gitlab.com/jideobs/nebularcore/tools/migrate"
 )
 
-func RunMigration(t *testing.T, baseDir, dataDir string) func(*testing.T) {
-	migrationDir := filepath.Join(baseDir, "test/data/migrations")
+func RunMigration(t *testing.T, migrationDir, dataDir string) func(*testing.T) {
 	migrationDir = filesystem.EncodeFilePathAsFileURL(migrationDir)
 	connectionString := fmt.Sprintf("sqlite://%s", filepath.Join(dataDir, "data.db"))
 	runner, err := migrate.NewRunner(migrationDir, connectionString)
